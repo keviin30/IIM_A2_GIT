eeb678cc7d08094be345724b650c9a069286d20b
CamCordier <camillecordier@MacBook-Pro-de-Camille.local>
2016-09-15 23:14:28 +0200

    dernière modif de add_music.php

--- add_music.php
+++ add_music.php
@@ -3,18 +3,16 @@ session_start();
 require('config/config.php');
 require('model/functions.fn.php');
 
-if( isset($_FILES['musics']) && !empty($_FILES['musics']) &&
+if( isset($_FILES['music']) && !empty($_FILES['music']) && 
 	isset($_POST['title']) && !empty($_POST['title'])){
-
-	$file = $_FILES['musics'];
-
+	
+	$file = $_FILES['music'];
 	if( 0 === $file['error']){
 		$ext = strtolower(substr(strrchr($file['name'], '.')  ,1));
 		// Vérification des extentions
-		if (1==1) {
+		if (preg_match('/\.(mp3|ogg)$/i', $file['name'])) {
 			$filename = md5(uniqid(rand(), true));
 			$destination = "musics/{$filename}.{$_SESSION['id']}.{$ext}";
-
 			// TODO
 			if(move_uploaded_file($file['tmp_name'], $destination)){
 				addMusic($db,$_SESSION['id'],$_POST['title'],$destination);
@@ -23,13 +21,11 @@ if( isset($_FILES['musics']) && !empty($_FILES['musics']) &&
 		} else {
 			$error = 'Erreur, le fichier n\'a pas une extension autorisée !';
 		}
-
 	}
-
-
 }
+
 include 'view/_header.php';
 include 'view/add_music.php';
 include 'view/_footer.php';
 
-?>
+
